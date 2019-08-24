d1f70c458dc9ec2bae3428b563a2b37bb5f47d80
m79932 <rsjo@tdc.dk>
2019-08-24 14:30:22 +0200

    cleanup

--- palindrome-service/src/main/java/palindrome/Application.java
+++ palindrome-service/src/main/java/palindrome/Application.java
@@ -1,6 +1,5 @@
 package palindrome;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -10,15 +9,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
-import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 @ComponentScan
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class Application {
 
-   // private Producer kafkaMessageProducer;
-    //private Consumer kafkaMessageConsumer;
 
     @Setter
     @Getter
@@ -27,31 +23,5 @@ public class Application {
     public static void main(String[] args) {
 
         SpringApplication.run(Application.class, args);
-        //ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-       // kafkaMessageProducer.sendPalindromeMessage(new Palindrome());
-        //kafkaMessageConsumer.latch.await(10, TimeUnit.SECONDS);
-        //setContext(context);
-        //context.close();
     }
-
- /*   public void sendKafkaMessage(Palindrome palindromeMessage){
-        try {
-            kafkaMessageProducer.sendPalindromeMessage(palindromeMessage);
-            kafkaMessageConsumer.latch.await(10, TimeUnit.SECONDS);
-            context.close();
-        }catch(InterruptedException exception){
-            exception.getStackTrace();
-        }
-    }*/
-
- /*   @Bean
-    public Producer messageProducer() {
-        return new Producer();
-    }
-
-    @Bean
-    public Consumer messageListener() {
-        return new Consumer();
-    }
-*/
 }
